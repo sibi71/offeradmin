7f4ddc59cc6cae4ce643a071888bded4f6462fea
sibi71 <thearunsibi@gmail.com>
2023-11-05 14:55:34 +0530

    first commit

---
+++ src/components/theme/ThemeSuspense.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ThemeSuspense = () => {
+  return (
+    <div className="w-full h-screen p-6 text-lg font-medium text-gray-600 dark:text-gray-400 dark:bg-gray-900 flex justify-center items-center">
+      <img src="https://i.gifer.com/ZKZg.gif" className="w-20 h-20"  />
+    </div>
+  );
+};
+
+export default ThemeSuspense;
